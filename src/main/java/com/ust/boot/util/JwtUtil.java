7e6a9d2b63513e0ff33f030966c502d3595f3696
G kausthub <KausthubRao.Garikapati@ust.com>
2024-08-12 12:38:46 +0530

    first

---
+++ src/main/java/com/ust/boot/util/JwtUtil.java
@@ -0,0 +1,69 @@
+package com.ust.boot.util;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+ //jwt.io
+ //cryptii.com
+
+//After this go to postmann...type localhost:8899 and click on post..then go to body..select raw and json and type{"username":"foo","password":"foo"}
+//Why only "foo" because we have used in the code 
+//U will get the jwt token...copy that token
+// then go to authorization in postmann..select the bearer token..paste the token without any double quotes etc..and then paste it over the side ..
+//then click the localhost:8899/greet or user or admin...without token it wont work
+@Service
+public class JwtUtil {
+
+	private String SECRET_KEY = "XXsecretXX";
+
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+	}
+
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userDetails.getUsername());
+	}
+
+	private String createToken(Map<String, Object> claims, String subject) {
+
+		return Jwts.builder().
+				setClaims(claims).
+				setSubject(subject).
+				setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+				.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+	}
+
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+}
